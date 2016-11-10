2a383915402e067107a9b4474bbdb6d9c418c8a1
BenHardin <bbhardin1@gmail.com>
2016-11-10 18:09:20 -0500

    Updates
    Began work on calculator project. Reviewed some basic JFrame functions.

--- PLTW-CSA_JFrame_Project/src/betaben/jframeprogject/Main.java
+++ PLTW-CSA_JFrame_Project/src/betaben/jframeprogject/Main.java
@@ -1,6 +1,8 @@
 package betaben.jframeprogject;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -10,30 +12,51 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		initScene();
+		
+	}
+	
+	private static void initScene() {
+		
+		String string = "";
+		
 		JFrame jframe = new JFrame();
+		jframe.setLayout(null);
+		jframe.setResizable(false);
 		jframe.setDefaultCloseOperation(jframe.EXIT_ON_CLOSE);
 		jframe.setTitle("Calculator");
 		jframe.setSize(720, 1280);
-		jframe.setBackground(Color.black);
-		jframe.setVisible(true);
+		jframe.setBackground(Color.lightGray);
 
 		JButton jbutton = new JButton();
-		jbutton.setLocation(5, 5);
-		jbutton.setSize(500, 500);
+		jbutton.setSize(100, 100);
+		jbutton.setText("1");
+		jbutton.setLocation(jframe.getWidth()/2, jframe.getHeight()/2);
 		jbutton.setBackground(Color.BLACK);
-		jbutton.setVisible(true);
 		
 		JLabel display = new JLabel();
 		display.setText("Hello World!");
-		display.setLocation(500, 500);
-		display.setSize(100, 100);
+		display.setLocation(0, 0);
+		display.setOpaque(false);
+		display.setSize(jframe.getWidth(), 100);
 		display.setForeground(Color.black);
 		display.setBackground(Color.black);
-		display.setVisible(true);
+		
+		jbutton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e)
+			  {
+			    display.setText("1");
+			  }
+		});
 		
 		jframe.add(jbutton);
 		jframe.add(display);
 		
+		jbutton.setVisible(true);
+		display.setVisible(true);
+		
+		jframe.setVisible(true);
+		
 	}
-
+	
 }
