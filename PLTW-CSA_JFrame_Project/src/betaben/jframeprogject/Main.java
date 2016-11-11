c4dfdab7789afe3a952e60dd232b25894ab3efe3
BenHardin <bbhardin1@gmail.com>
2016-11-11 10:09:07 -0500

    Updates
    Calculator work.

--- PLTW-CSA_JFrame_Project/src/betaben/jframeprogject/Main.java
+++ PLTW-CSA_JFrame_Project/src/betaben/jframeprogject/Main.java
@@ -3,6 +3,7 @@ package betaben.jframeprogject;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -20,19 +21,25 @@ public class Main {
 		
 		String string = "";
 		
+		Random random = new Random();
+		
 		JFrame jframe = new JFrame();
 		jframe.setLayout(null);
 		jframe.setResizable(false);
 		jframe.setDefaultCloseOperation(jframe.EXIT_ON_CLOSE);
 		jframe.setTitle("Calculator");
-		jframe.setSize(720, 1280);
+		jframe.setSize(1280, 720);
 		jframe.setBackground(Color.lightGray);
 
-		JButton jbutton = new JButton();
-		jbutton.setSize(100, 100);
-		jbutton.setText("1");
-		jbutton.setLocation(jframe.getWidth()/2, jframe.getHeight()/2);
-		jbutton.setBackground(Color.BLACK);
+		JButton oneButton = new JButton();
+		oneButton.setSize(100, 100);
+		oneButton.setText("1");
+		oneButton.setLocation(jframe.getWidth()/2, jframe.getHeight()/2);
+		
+		JButton twoButton = new JButton();
+		twoButton.setSize(100, 100);
+		twoButton.setText("2");
+		twoButton.setLocation(jframe.getWidth()/3, jframe.getHeight()/3);
 		
 		JLabel display = new JLabel();
 		display.setText("Hello World!");
@@ -42,20 +49,32 @@ public class Main {
 		display.setForeground(Color.black);
 		display.setBackground(Color.black);
 		
-		jbutton.addActionListener(new ActionListener(){
+		oneButton.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e)
 			  {
 			    display.setText("1");
 			  }
 		});
 		
-		jframe.add(jbutton);
+		twoButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				display.setText("2");
+			}
+		});
+		
+		jframe.add(oneButton);
+		jframe.add(twoButton);
 		jframe.add(display);
 		
-		jbutton.setVisible(true);
+		oneButton.setVisible(true);
+		twoButton.setVisible(true);
 		display.setVisible(true);
 		
 		jframe.setVisible(true);
+		
+//		while(true){
+//			jframe.getContentPane().setBackground(new Color(random.nextInt(0xFFFFFF)));
+//		}
 		
 	}
 	
